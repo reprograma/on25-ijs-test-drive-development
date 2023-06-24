5423d7b40c0b92fe01be0e79f787d1755661eae5
Mariana de Lima Santos <mariana>
2023-06-24 02:54:35 -0300

    adiciona arquivos de aula

---
+++ atividades/para-sala/TDD - atividades/fibonacci.test.js
@@ -0,0 +1,3 @@
+/* Escreva um teste para uma função que dado um índice (0) e o tamanho do array,
+ retorne a Sequência de Fibonacci iniciando no índice e finalizando depois do tamanho
+especificado para o array */
---
+++ atividades/para-sala/TDD - atividades/soma.test.js
@@ -0,0 +1,2 @@
+
+// A função soma dois números, caso os números sejam iguais, a soma é triplicada
