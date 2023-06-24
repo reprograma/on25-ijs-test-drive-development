e3e8635d77e90798865461628e9a2eba76de6b5e
Deborah Gomes Souza <deborah270691@gmail.com>
2023-06-24 18:08:27 -0300

    o fibonacci incomoda muita gente

--- atividades/para-sala/TDD - atividades/revertString.js
+++ atividades/para-sala/TDD - atividades/revertString.js
@@ -9,3 +9,14 @@ function reverse(str) {
   }
 
 module.exports = reverse
+function revertString(str) {
+    let word = '';
+    
+    for (let i = str.length - 1; i >= 0; i--) {
+      word += str[i];
+    }
+    
+    return word;
+  }
+   
+  module.exports = revertString
