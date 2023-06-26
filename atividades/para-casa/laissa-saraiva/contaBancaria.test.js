1ac02dfbe524c1cc54a20608bc7b42c6bcdd34ff
Laissa Cunha <laissa.cunha@ed3.com.br>
2023-06-26 12:25:12 -0300

    feat:ajusta a sequência e reescreve a descrição dos testes

--- atividades/para-casa/laissa-saraiva/contaBancaria.test.js
+++ atividades/para-casa/laissa-saraiva/contaBancaria.test.js
@@ -1,38 +1,49 @@
-// Considere um objeto que represente uma conta bancária, a conta possui o nome do titular, o saldo e o limite da conta. 
+// Considere um objeto que represente uma conta bancária, a conta possui o nome do titular, o saldo e o limite da conta.
 
-// É possível fazer operações de consulta de saldo, saque e depósito. 
+// É possível fazer operações de consulta de saldo, saque e depósito.
 
-// No caso de saque é necessário verificar se existe saldo suficiente para retirada, caso o cliente não tenha saldo suficiente para a operação, mas possua limite disponível (e suficiente), o saque poderá ocorrer, nesses casos o saldo do cliente ficará negativo após o saque. 
+// No caso de saque é necessário verificar se existe saldo suficiente para retirada, caso o cliente não tenha saldo suficiente para a operação, mas possua limite disponível (e suficiente), o saque poderá ocorrer, nesses casos o saldo do cliente ficará negativo após o saque.
 
-// Além disso, o limite de uma conta pode ser reajustado (para mais e para menos) ou desativado. 
+// Além disso, o limite de uma conta pode ser reajustado (para mais e para menos) ou desativado.
 
 // Use a abordagem Red - Green - Refactor para desenvolver essa aplicação.
 
-// import
-const {withdrawMoney, accountBalance, depositCash, adjustAccountLimit} = require('./contaBancaria');
-describe("Tests in Bank account", ()=> {
-	it("should verify if the withdrawMoney() is working ", ()=> {
-		expect(withdrawMoney(100)).toEqual(1900)
-	})
+const {withdrawMoney, accountBalance, depositCash, adjustAccountLimit,} = require("./contaBancaria");
+describe("Tests in Bank account", () => {
+  it("should show the account balance R$2000", () => {
+    expect(accountBalance("balance")).toEqual(
+      "O saldo atual de sua conta é R$2000."
+    );
+  });
 
-  it("should test the show the account balance", ()=> {
-		expect(accountBalance("balance")).toEqual("O saldo atual de sua conta é R$1900.");
-	})
+  it("should withdraw R$500 and return R$1500", () => {
+    expect(withdrawMoney(500)).toEqual(1500);
+  });
 
-  it("should test if the depositCash() is working", ()=> {
-		expect(depositCash(1000)).toEqual(2900);
-	})
+  it("should withdraw R$2000 and return R$-500", () => {
+    expect(withdrawMoney(2000)).toEqual(-500);
+  });
 
-  // Além disso, o limite de uma conta pode ser reajustado (para mais e para menos) ou desativado. 
-  it("should increase the mount of account limmit", ()=> {
+  it("should return *Unauthorized operation* because there is no money available", () => {
+    expect(withdrawMoney(5000)).toEqual("Unauthorized operation.");
+  });
+
+  it("should deposit R$1000 and return R$500", () => {
+    expect(depositCash(1000)).toEqual(500);
+  });
+
+  // Além disso, o limite de uma conta pode ser reajustado (para mais e para menos) ou desativado.
+  it("should increase R$10 in the account limit and return R$3010", () => {
     expect(adjustAccountLimit("increase", 10)).toEqual(3010);
-  })
-  
-  it("should decrease the mount of account limmit", ()=> {
-    expect(adjustAccountLimit("decrease", 10)).toEqual(3000);
-  })
+  });
 
-  it("should remove account limit from the object ", ()=> {
-    expect(adjustAccountLimit("cancel")).toEqual("Seu Limite adicional foi desativado.");
-  })
-})
+  it("should decrease R$50 in the account limit and return R$2960", () => {
+    expect(adjustAccountLimit("decrease", 50)).toEqual(2960);
+  });
+
+  it("should remove account limit from the object and return 'Seu Limite adicional foi desativado' ", () => {
+    expect(adjustAccountLimit("cancel")).toEqual(
+      "Seu Limite adicional foi desativado."
+    );
+  });
+});
