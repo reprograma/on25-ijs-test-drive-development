5da38b701ce1aa740ff7a7056c95da30a6159b2a
hinale <leticiaamarallara@hotmail.com>
2023-06-27 20:39:55 -0300

    teste em aula - fibonacci

--- atividades/para-sala/TDD - atividades/fibonacci.js
+++ atividades/para-sala/TDD - atividades/fibonacci.js
@@ -1,7 +1,13 @@
-const fibonacci = (i) => {
-  if(i <= 1) return 1;
+function fibonacciSeq(index, tam) {
+  const fibonacci = [0, 1];
 
-  return fibonacci(i - 1) + fibonacci(i - 2)
+  for(let i = 2; i <= index + tam; i++) {
+    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2]
+  }
+
+  return fibonacci.slice(index, index + tam)
 }
 
-module.exports = fibonacci
+console.log(fibonacciSeq(6, 7))
+
+module.exports = fibonacciSeq
