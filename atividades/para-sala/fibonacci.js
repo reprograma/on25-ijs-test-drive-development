01d561a1600717d119b6bc7cc27312662461a6b2
Ellie <elliekonuma@gmail.com>
2023-06-28 10:01:35 -0300

    fibonacci resolução

--- atividades/para-sala/fibonacci.js
+++
@@ -1,8 +0,0 @@
-const fibonacciSeq = require("./fibonacci");
-
-describe("fibonacci function", () => {
-  it("check fibonacci", () => {
-    const output = [8, 13, 21, 34, 55, 89, 144];
-    expect(fibonacciSeq(6, 7).toEqual(output));
-  });
-});
