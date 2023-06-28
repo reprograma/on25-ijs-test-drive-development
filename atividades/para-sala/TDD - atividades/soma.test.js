03288450cd51d1b89e4e303d67a64e59fa3c3837
Jessica Rafaeli Baron <jessica.baron@penseavanti.com.br>
2023-06-28 16:02:18 -0300

    bankAccout exercise

--- atividades/para-sala/TDD - atividades/soma.test.js
+++ atividades/para-sala/TDD - atividades/soma.test.js
@@ -1,7 +1,7 @@
 // A função soma dois números, caso os números sejam iguais, a soma é triplicada
 const sum = require('./soma')
 
-describe("sum two numbers", ()=>{
+describe("sum two number", ()=>{
     it('sum of 2 and 3 = 5', ()=>{
         expect(sum(2,3)).toEqual(5)
     })
